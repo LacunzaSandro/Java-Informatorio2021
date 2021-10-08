fff09b5b527f36a19adba32e2c01cb2098e58dd7
Sandro Lacunza <sandrolacunza@gmail.com>
2021-10-08 12:41:47 -0300

    Exercise 1

---
+++ EjerciciosComplementariosLevel1/Ejercicio 1 - Saludando/src/com/informatorio/Main.java
@@ -0,0 +1,12 @@
+package com.informatorio;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Introduzca su nombre: ");
+                String user = scan.next();
+                System.out.println("Hola " + user);
+            }
+        }
+
